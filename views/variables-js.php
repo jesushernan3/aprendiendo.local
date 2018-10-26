9d9e55618fe8650da8983b2f5da89b1481c8ee02
jesus <jesushernan3@gmail.com>
2018-10-25 22:58:04 -0300

    tercer commit

--- views/variables-js.php
+++ views/variables-js.php
@@ -1,11 +1,12 @@
 <?php 
 $raiz = "../";
-include_once ("../php/funciones-comunes.php");
+include_once("../php/funciones-comunes.php");
 $titulo_pagina = 'Variables JS';
-include ($raiz."inc/head.php");
-include ($raiz."inc/foot.php");
+include($raiz . "inc/head.php");
 ?>
 
 <main>
-  contenido
+  <h1>variales</h1>
 </main>
+
+<?php include($raiz . "inc/foot.php"); ?>
