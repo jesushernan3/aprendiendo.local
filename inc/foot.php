5544722e5511b5ea9df4206bf94d8b9a7f8a3f74
jesus <jesushernan3@gmail.com>
2018-10-26 22:51:51 -0300

    5to commit creo desde asus

--- inc/foot.php
+++ inc/foot.php
@@ -1,4 +1,5 @@
 	<?php include 'partials/footer.php'; ?>
-	 <script src="js/main.js"></script>
+	 <script src="<?php echo $raiz; ?>js/main.js"></script>
+	 <script src="<?php echo $raiz; ?>js/prism.js"></script>
 	</body>
 </html>
--- inc/head.php
+++ inc/head.php
@@ -3,11 +3,13 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="with=device-with, initial-scale=1, maximum-scale=1">
-		<link rel="stylesheet" href="<?php echo $raiz;?>css/main.css">
+		<link rel="stylesheet" href="<?php echo $raiz; ?>css/main.css">
+		<link rel="stylesheet" href="<?php echo $raiz; ?>css/prism.css">
+		<link rel="stylesheet" href="<?php echo $raiz; ?>fonts/operator-mono.css">
 
 		<title>
-		<?php parametro_plantilla("titulo_pagina");	?>		
+		<?php parametro_plantilla("titulo_pagina"); ?>		
 		</title>
 	</head>
-	<body <?php idBody("id_body");?>>
-	<?php include ('partials/header.php'); ?>
+	<body <?php idBody("id_body"); ?>>
+	<?php include('partials/header.php'); ?>
