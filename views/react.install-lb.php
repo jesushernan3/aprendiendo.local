b46be59a9876dc4deffebc937e0e79388ee111b4
jesus <jesushernan3@gmail.com>
2019-02-06 13:42:49 -0300

    nuevo contenido

--- views/react.install-lb.php
+++ views/react.install-lb.php
@@ -41,6 +41,31 @@ En este caso, agrega 2 etiquetas de secuencia de comandos al final de la etiquet
     >&lt/script>
     &lt/body>
     &lt/html></code></pre>
+  
+  <p>Después de esas etiquetas, puede cargar sus archivos JavaScript que usan React, o incluso JavaScript en línea en una etiqueta de script:</p>
+  <pre class="language-html" rel="html"><code class="language-html">&ltscript src="app.js">&lt/script>
+
+  &lt!-- or -->
+
+  &ltscript>
+  //my app
+  &lt/script></code></pre>
+
+  <p>Para usar JSX necesitas un paso adicional: cargar Babel</p>
+  <pre class="language-html" rel="html"><code class="language-html">&ltscript src="https://unpkg.com/babel-standalone@6/babel.min.js">&lt/script></code></pre>
+
+  <p>y cargue sus scripts con el <code>texto/babel</code> MIME type:</p>
+  <pre class="language-html" rel="html"><code class="language-html">&ltscript src="app.js" type="text/babel">&lt/script></code></pre>
+
+  <p>Ahora puede agregar JSX en su archivo app.js:</p>
+  <pre class="language-js" rel="js"><code class="language-js">const Button = () => {
+  return &ltbutton>Click me!&lt/button>
+}
+
+ReactDOM.render(&ltButton />, document.getElementById('root'))</code></pre>
+
+<p>Comenzar de esta manera con las etiquetas de script es bueno para crear prototipos y permite un inicio rápido sin tener que configurar un flujo de trabajo complejo.</p>
+
    
 
 </main>
