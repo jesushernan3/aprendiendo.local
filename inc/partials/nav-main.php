cb7a68bbdf04f2dc51551068de309a1a025955e1
Jesus Hernan <jesushernan3@gmail.com>
2020-04-10 21:45:10 -0300

    ms pruebas para menu mobile, aunque no anda ni en firefox ni en brave

--- inc/partials/nav-main.php
+++ inc/partials/nav-main.php
@@ -1,119 +1,28 @@
-<nav id="main-nav" class="navbar container" role="navigation">
-
-	
-	<div class="navbar-brand">
-    <a class="navbar-item" href="/"><em>APRENDIENDO</em></a>
-	</div>
-	
-	<div class="navbar-menu">
-		<div class="navbar-start nav-list">
-
-			<a href="#" class="navbar-item">HTML</a>
-		
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">CSS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>	
-
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">JS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<div class="navbar-item" href="">JavaScript Base</div>
-					<a class="navbar-item" href="/views/sintaxys-reglas-js.php">Sintaxys & Reglas</a>
-					<a class="navbar-item" href="/views/variables-js.php">Variables</a>
-					<a class="navbar-item" href="/views/tipos-js.php">Tipos</a>
-					<a class="navbar-item" href="#">prueba 3</a>
-					<a class="navbar-item" href="/views/comentar-js.php">Comentar prueba largo</a>
-					<a class="navbar-item" href="/views/operadores-js.php">Operadores</a>
-					<a class="navbar-item" href="/views/webpack-js.php">Webpack</a>
-					<a class="navbar-item" href="#">submenu 4</a>
-					<a class="navbar-item" href="#">submenu 5</a>
-				</div>
-			</div>
-			
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">PHP</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>		
-
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">FRAMEWORKS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>		
-			
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">LIBRERIAS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>		
-
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">CONCEPTOS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>		
-
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">SNIPPETS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>		
-
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#" class="navbar-link is-arrowless">REACT JS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>		
-
-			<div class="navbar-item has-dropdown is-hoverable">
-				<a href="#!" class="navbar-link is-arrowless">NOTAS</a>
-				<div class="navbar-dropdown nav-dropdown">
-					<a class="navbar-item" href="#">submenu 1</a>
-					<a class="navbar-item" href="#">submenu 2</a>
-					<a class="navbar-item" href="#">submenu 3</a>
-				</div>
-			</div>
-
-		</div>
-
-		<div class="navbar-end">
-			
-			
-		 <!-- <li class="nabvar-item">
-				<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-					<span aria-hidden="true"></span>
-					<span aria-hidden="true"></span>
-					<span aria-hidden="true"></span>
-				</a>
-			</li>  -->
-		</div class="navbar-end"
-	</div>
-	
-</nav>
-<div class="nav-mobile">
-				<a id="nav-toggle" href="#!"><span></span></a>
-			</div>	
+<div class="brand">
+	<a href="https://webdevtrick.com/"><img src="https://webdevtrick.com/wp-content/uploads/logo-fb-1.png"></a>
+</div>
+<nav>
+      <div class="nav-mobile">
+        <a id="nav-toggle" href="#!"><span></span></a>
+      </div>
+      <ul class="nav-list">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Web Design</a></li>
+        <li><a href="#">Web Development</a>
+          <ul class="nav-dropdown">
+            <li><a href="#">HTML</a></li>
+            <li><a href="#">CSS</a></li>
+            <li><a href="#">JavaScript</a></li>
+          </ul>
+        </li>
+        <li><a href="#!">Graphic Design</a>
+			    <ul class="nav-dropdown">
+            <li><a href="#">Photoshop</a></li>
+            <li><a href="#">Illustrator</a></li>
+            <li><a href="#">InDesign</a></li>
+          </ul>
+        </li>
+        <li><a href="#!">SEO</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </nav>
--- js/main.js
+++ js/main.js
@@ -50,28 +50,9 @@
 
 // Menu Mobile
 /** code by webdevtrick ( https://webdevtrick.com )  ORIGINAL**/
-// (function($) { 
-//   $(function() { 
-//     $('nav ul li a:not(:only-child)').click(function(e) {
-//       $(this).siblings('.nav-dropdown').toggle();
-//       $('.dropdown').not($(this).siblings()).hide();
-//       e.stopPropagation();
-//     });
-//     $('html').click(function() {
-//       $('.nav-dropdown').hide();
-//     });
-//     $('#nav-toggle').click(function() {
-//       $('nav ul').slideToggle();
-//     });
-//     $('#nav-toggle').on('click', function() {
-//       this.classList.toggle('active');
-//     });
-//   }); 
-// })(jQuery);
-
 (function($) { 
   $(function() { 
-    $('nav div div a:not(:only-child)').click(function(e) {
+    $('nav ul li a:not(:only-child)').click(function(e) {
       $(this).siblings('.nav-dropdown').toggle();
       $('.dropdown').not($(this).siblings()).hide();
       e.stopPropagation();
@@ -80,7 +61,7 @@
       $('.nav-dropdown').hide();
     });
     $('#nav-toggle').click(function() {
-      $('nav div').slideToggle();
+      $('nav ul').slideToggle();
     });
     $('#nav-toggle').on('click', function() {
       this.classList.toggle('active');
